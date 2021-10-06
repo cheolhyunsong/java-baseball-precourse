8172c1823dfad805e9f3b93cbf679e271a6486af
CheolHyun Song <chy.song89@gmail.com>
2021-10-06 20:38:48 +0900

    feat(Umpire): 승리(3스트라이크) 여부 체크 기능

--- src/main/java/baseball/Umpire.java
+++ src/main/java/baseball/Umpire.java
@@ -34,6 +34,10 @@ public class Umpire {
     }
 
     public Score getScore() {
-        return this.score;
+        return score;
+    }
+
+    public boolean isWin() {
+        return score.getStrike() == Const.MAX_DIGIT;
     }
 }
