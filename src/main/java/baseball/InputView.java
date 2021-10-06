bd0c9219b21b4c023d945d09ac20b807afebe9d7
CheolHyun Song <chy.song89@gmail.com>
2021-10-06 21:20:36 +0900

    refactor: 입/출력 클래스 불필요한 코드 개선
    - InputView: 불필요한 코드 삭제
    - outputView: 코드 10줄 초과로 함수 분리 처리

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -4,18 +4,14 @@ import nextstep.utils.Console;
 
 public class InputView {
     public static String printPitchingQustion() {
-        String inputData;
         System.out.print("숫자를 입력해주세요 : ");
-        inputData = Console.readLine();
 
-        return inputData;
+        return Console.readLine();
     }
 
     public static String printContinuousQustion() {
-        String inputData;
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        inputData = Console.readLine();
 
-        return inputData;
+        return Console.readLine();
     }
 }
--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -2,35 +2,47 @@ package baseball;
 
 public class OutputView {
 
-    private static String getScoreMessage(Score score) {
-        String resultMessage = "";
-
+    private static String getResultMessage(Score score) {
         if (score.getStrike() == 0 && score.getBall() == 0) {
            return "낫싱";
         }
 
+        return getScoreMessage(score);
+    }
+
+    private static String getScoreMessage(Score score) {
+        StringBuilder resultBuilder = new StringBuilder();
+
+        resultBuilder.append(getStrikeMessage(score));
+        if (resultBuilder.length() > 0) {
+            resultBuilder.append(" ");
+        }
+        resultBuilder.append(getBallMessage(score));
+
+        return resultBuilder.toString();
+    }
+
+    private static String getStrikeMessage(Score score) {
         if (score.getStrike() > 0) {
-            resultMessage = String.format("%d스트라이크", score.getStrike());
+            return score.getStrike() + "스트라이크";
         }
 
-        if (resultMessage.length() > 0) {
-            resultMessage += " ";
-        }
+        return "";
+    }
 
+    private static String getBallMessage(Score score) {
         if (score.getBall() > 0) {
-            resultMessage += String.format("%d볼", score.getBall());
+            return score.getBall() + "볼";
         }
 
-        return resultMessage;
+        return "";
     }
 
     public static void printResult(Score score) {
-        String resultMessage = getScoreMessage(score);
-
-        System.out.println(resultMessage);
+        System.out.println(getResultMessage(score));
     }
 
-    public static void getComplate() {
+    public static void printComplete() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
     }
 }
