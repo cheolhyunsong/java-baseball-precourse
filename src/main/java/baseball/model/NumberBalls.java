e2be87fe0b136858dd91b739efd4c90f029270c7
songsimo <chy.song89@gmail.com>
2022-04-14 02:12:05 +0900

    feat: 유효한 범위의 숫자인지 검증 구현

--- src/main/java/baseball/model/NumberBalls.java
+++ src/main/java/baseball/model/NumberBalls.java
@@ -1,10 +1,24 @@
 package baseball.model;
 
+import java.util.Objects;
+
 public class NumberBalls {
 	public static int CIPHER = 3;
+	public static final String NUMBER_REG_EX = "^[1-9]*$";
 
 	public NumberBalls(String pitchingNumber) {
+		validNumber(pitchingNumber);
 		validCipher(pitchingNumber);
+	}
+
+	private void validNumber(String pitchingNumber) {
+		if(Objects.isNull(pitchingNumber)) {
+			throw new IllegalArgumentException("입력값이 올바르지 않습니다.");
+		}
+
+		if(!pitchingNumber.matches(NUMBER_REG_EX)) {
+			throw new IllegalArgumentException("입력값이 올바르지 않습니다.");
+		}
 	}
 
 	private void validCipher(String pitchingNumber) {
