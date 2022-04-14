16170683279ffb282dc8babc4f901175c6804cce
songsimo <chy.song89@gmail.com>
2022-04-15 00:40:36 +0900

    feat: 중복된 숫자인지 검증 구현

--- src/main/java/baseball/model/NumberBalls.java
+++ src/main/java/baseball/model/NumberBalls.java
@@ -1,14 +1,24 @@
 package baseball.model;
 
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class NumberBalls {
 	public static int CIPHER = 3;
 	public static final String NUMBER_REG_EX = "^[1-9]*$";
 
+	private Set<Character> pitchingSet = new LinkedHashSet<>();
+
 	public NumberBalls(String pitchingNumber) {
 		validNumber(pitchingNumber);
 		validCipher(pitchingNumber);
+
+		for(char charNum : pitchingNumber.toCharArray()) {
+			pitchingSet.add(charNum);
+		}
+
+		validDuplication(pitchingNumber);
 	}
 
 	private void validNumber(String pitchingNumber) {
@@ -24,6 +34,12 @@ public class NumberBalls {
 	private void validCipher(String pitchingNumber) {
 		if(pitchingNumber.length() != CIPHER) {
 			throw new IllegalArgumentException(String.format("유효하지 않은 자릿수입니다(%s 자릿수).", CIPHER));
+		}
+	}
+
+	private void validDuplication(String pitchingNumber) {
+		if(pitchingSet.size() != CIPHER) {
+			throw new IllegalArgumentException("입력값이 올바르지 않습니다.");
 		}
 	}
 }
