6f9fed75a5cc9ba2db94ade7fffdc7572f75ecc8
songsimo <chy.song89@gmail.com>
2022-04-16 13:44:17 +0900

    test: Strike 판정 테스트 작성

--- src/test/java/baseball/model/BallTest.java
+++ src/test/java/baseball/model/BallTest.java
@@ -1,6 +1,6 @@
 package baseball.model;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,5 +23,14 @@ class BallTest {
 			Ball ball = new Ball("a");
 		}).isInstanceOf(IllegalArgumentException.class)
 			.hasMessageContaining("입력값이 올바르지 않습니다.");
+	}
+
+	@Test
+	@DisplayName("스트라이크 검증")
+	void strike() {
+		Ball ball = new Ball(2, 3);
+		BallStatus ballStatus = ball.call(new Ball(2, 3));
+
+		assertThat(ballStatus).isEqualTo(BallStatus.STRIKE);
 	}
 }
